2fc53e679faf6e5f894e8aef8fcc5c55d875d194
ZhangChenglong <ZhangChenglong@hdsc.com>
2018-03-22 19:34:40 +0800

    [add]完善项目3

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,24 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegistrationForm(forms.ModelForm):
+    # username = forms.CharField(label="username", widget=forms.TextInput)
+    # email = forms.CharField(label="email", widget=forms.EmailInput)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="confirm Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model: User
+        fields = ("username", "email")
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('password do not match')
+        return cd['password2']
