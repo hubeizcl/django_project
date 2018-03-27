bc4dac85b8b88d38913def603c7e1f0d4bcfbd54
ZhangChenglong <ZhangChenglong@hdsc.com>
2018-03-27 21:31:54 +0800

    [add]完善项目14

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^article_column/$', views.article_column, name="article_column"),
-]
+    url(r'^rename_article_column/$', views.rename_article_column, name="rename_article_column"),
+    url(r'^delete_article_column/$', views.delete_article_column, name="delete_article_column"), ]
 app_name = 'article'
--- article/views.py
+++ article/views.py
@@ -1,11 +1,51 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import ArticleColumn
+from .form import ArticleColumnForm
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 
 @login_required(login_url='/account/login/')
+@csrf_exempt
 def article_column(request):
-    columns = ArticleColumn.objects.filter(user=request.user)  # 根据当前用户筛选出和当前用户一直的文章的元组
-    return render(request, 'article/column/article_column.html', {"columns": columns})
+    if request.method == "GET":
+        columns = ArticleColumn.objects.filter(user=request.user)  # 根据当前用户筛选出和当前用户一直的文章的元组
+        column_form = ArticleColumnForm()
+        return render(request, 'article/column/article_column.html', {"columns": columns, "column_form": column_form})
+    if request.method == "POST":
+        column_name = request.POST['column']
+        columns = ArticleColumn.objects.filter(user_id=request.user.id, column=column_name)
+        if columns:
+            return HttpResponse('2')
+        else:
+            ArticleColumn.objects.create(user=request.user, column=column_name)
+            return HttpResponse('1')
+
+
+@login_required(login_url='/account/login/')
+@csrf_exempt
+def rename_article_column(request):
+    column_name = request.POST['column_name']
+    column_id = request.POST['column_id']
+    try:
+        line = ArticleColumn.objects.get(id=column_id)
+        line.column = column_name
+        line.save()
+        return HttpResponse('1')
+    except:
+        return HttpResponse('0')
+
+
+@login_required(login_url='/account/login/')
+@csrf_exempt
+def delete_article_column(request):
+    column_id = request.POST['column_id']
+    try:
+        line = ArticleColumn.objects.get(id=column_id)
+        line.delete()
+        return HttpResponse('1')
+    except:
+        return HttpResponse('0')
